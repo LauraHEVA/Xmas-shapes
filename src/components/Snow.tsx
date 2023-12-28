0d73a39291f853517cdca424210c0097f3a1ddb8
LauraHEVA <lhvarela@gmail.com>
2023-12-29 00:16:47 +0100

    Add toggle to snow logo

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -1,12 +1,21 @@
-import logo from '../logo.svg';
 import React from 'react';
+import SnowSvg from './SnowSvg.tsx';
 
-const Snow = ({reverse=false, hidden=false}) => {
+const Snow = ({ reverse = false }) => {
+  const green = '#4f772d';
+  const white = '#e0e1dd';
+  const [state, setState] = React.useState({ color: white });
+
+  const changeColor = () => {
+    const newColor = state.color === green ? white : green;
+    setState({ color: newColor });
+  };
+
   return (
-    <div className="snow-wrapper" hidden={hidden}>
-    <img src={logo} className={`App-logo${reverse ? "-reverse" : ""}`} alt="logo" />
+    <div className="snow-wrapper" onClick={changeColor}>
+      <SnowSvg reverse={reverse} fillColor={state.color}/>
     </div>
-  )
-}
+    );
+  };
 
 export default Snow;
