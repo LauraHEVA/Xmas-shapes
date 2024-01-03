4985e6c0fcd766c17281196e8f84c4c414184685
LauraHEVA <lhvarela@gmail.com>
2024-01-03 14:37:34 +0100

    Add poke-img in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import Snow from './Snow.tsx';
+import pokeMario from '../utils/poke-mario.png';
 
 const Header = () => {
     return (
-        <header className="App-header">
-            <Snow reverse={true}/>
-            <Snow/>
-            <Snow reverse={true}/>
-            <Snow disableMobile={true}/>
-            <Snow reverse={true} disableMobile={true}/>
-            <Snow disableMobile={true}/>
-            <Snow reverse={true} disableMobile={true}/>
-            <Snow disableMobile={true}/>
-            <Snow reverse={true} disableMobile={true}/>
-        </header>
+        <div className='header-container' >
+            <img src={pokeMario} alt="poke-mario" className="poke-mario" height={80} width={80}/>
+            <header className="App-header">
+                <Snow reverse={true}/>
+                <Snow/>
+                <Snow reverse={true}/>
+                <Snow disableMobile={true}/>
+                <Snow reverse={true} disableMobile={true}/>
+                <Snow disableMobile={true}/>
+                <Snow reverse={true} disableMobile={true}/>
+                <Snow disableMobile={true}/>
+                <Snow reverse={true} disableMobile={true}/>
+            </header>
+            <p className="header-text">
+                Click the snowflakes!
+            </p>
+        </div>
     );
 }
 
